74fbfda614f3038e4e2dcea70ad185a6141c78da
Navrit Bal <navritbal@gmail.com>
2016-04-11 18:47:39 +0100

    Unit callbacks and plain colours

--- Android/DoseNet/app/src/main/java/com/navrit/dosenet/MainActivity.java
+++ Android/DoseNet/app/src/main/java/com/navrit/dosenet/MainActivity.java
@@ -1,20 +1,31 @@
 package com.navrit.dosenet;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    public String unit_selected = "usv";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("Unit: ", unit_selected);
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
@@ -38,8 +49,26 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             case R.id.action_settings:
-                Intent settings_intent = new Intent(this, SettingsActivity.class);
-                startActivity(settings_intent);
+                new MaterialDialog.Builder(this)
+                        .title(R.string.title)
+                        .content(R.string.content)
+                        .positiveText(R.string.agree)
+                        .negativeText(R.string.disagree)
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                unit_selected = "usv";
+                                Log.e("Unit: ", unit_selected);
+                            }
+                        })
+                        .onNegative(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                unit_selected = "mrem";
+                                Log.e("Unit: ", unit_selected);
+                            }
+                        })
+                        .show();
                 return true;
 
             default:
