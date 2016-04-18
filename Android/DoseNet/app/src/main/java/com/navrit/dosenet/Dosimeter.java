1d02be0c8a2ad04e1332ffaab5ba8b71932a038a
Navrit Bal <navritbal@gmail.com>
2016-04-18 11:45:19 +0100

    Optional lon/lat constructor

--- Android/DoseNet/app/src/main/java/com/navrit/dosenet/Dosimeter.java
+++ Android/DoseNet/app/src/main/java/com/navrit/dosenet/Dosimeter.java
@@ -7,20 +7,31 @@ public class Dosimeter {
     String unitSelected;
     String lastTime;
     //double distance;
-    //double lat;
-    //double lon;
+    double lat;
+    double lon;
 
     // Image (seal/crest) for card view
 
     //List<double> doses_uSv; // For dose over time plots
     //List<String> times;
 
-    Dosimeter(String name, double lastDose_uSv, String unitSelected, String lastTime){
+    public Dosimeter(String name, double lastDose_uSv, String unitSelected, String lastTime){
         this.name = name;
         this.lastDose_uSv = lastDose_uSv;
         this.lastDose_mRem = lastDose_uSv/10;
         this.unitSelected = unitSelected;
         this.lastTime = lastTime;
+    }
+
+    public Dosimeter(String name, double lastDose_uSv, String unitSelected, String lastTime,
+                     double lon, double lat){
+        this.name = name;
+        this.lastDose_uSv = lastDose_uSv;
+        this.lastDose_mRem = lastDose_uSv/10;
+        this.unitSelected = unitSelected;
+        this.lastTime = lastTime;
+        this.lon = lon;
+        this.lat = lat;
     }
 
 }
